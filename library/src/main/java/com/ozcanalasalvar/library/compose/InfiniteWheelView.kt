5d80c9a8ab2d5619e6c0ffc1aa07192d91263f11
ozcanalasalvar <ozcanalasalvarr@gmail.com>
2023-09-19 23:39:06 +0300

    scroll logic fix

--- library/src/main/java/com/ozcanalasalvar/library/compose/InfiniteWheelView.kt
+++ library/src/main/java/com/ozcanalasalvar/library/compose/InfiniteWheelView.kt
@@ -2,7 +2,6 @@ package com.ozcanalasalvar.library.compose
 
 import android.util.Log
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
@@ -16,6 +15,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.DpSize
 import androidx.compose.ui.unit.dp
 import kotlinx.coroutines.launch
@@ -41,7 +41,7 @@ fun InfiniteWheelView(
     LaunchedEffect(key1 = isScrollInProgress) {
         coroutineScope.launch {
             if (!isScrollInProgress) {
-                calculateSnappedItemIndex(lazyListState)?.let {
+                calculateSnappedItemIndex(lazyListState, size.height).let {
                     if (lazyListState.firstVisibleItemScrollOffset != 0) lazyListState.animateScrollToItem(
                         it, 0
                     )
@@ -74,7 +74,10 @@ fun InfiniteWheelView(
                     modifier = Modifier
                         .height(size.height / rowCount)
                         .width(size.width),
-                    contentAlignment = Alignment.Center
+//                        .graphicsLayer {
+//                            this.rotationX = calculateItemRotation(lazyListState, itemCount, it)
+//                        },
+                    contentAlignment = Alignment.Center,
                 ) {
 
                     Text(text = (it % itemCount + 1).toString())
@@ -90,12 +93,12 @@ fun InfiniteWheelView(
 }
 
 
-private fun calculateSnappedItemIndex(listState: LazyListState): Int {
+private fun calculateSnappedItemIndex(listState: LazyListState, height: Dp): Int {
     val currentItem = listState.layoutInfo.visibleItemsInfo.firstOrNull()
     var index = currentItem?.index ?: 0
 
     if (currentItem?.offset != 0) {
-        if (currentItem != null) {
+        if (currentItem != null && currentItem.offset <= -height.value * 4 / 10) {
             index++
         }
     }
@@ -107,19 +110,23 @@ private fun calculateItemRotation(listState: LazyListState, itemCount: Int, inde
     val firstVisibleItem = listState.firstVisibleItemIndex
     val spannedIndex =
         firstVisibleItem - 1 + (listState.layoutInfo.visibleItemsInfo.size / 2).toInt()
+   // Log.d("spanned","$spannedIndex")
+    Log.d("spanned","${(spannedIndex - index )}")
 
-
-    val rotationX = -15f * (spannedIndex - index)
+//    val rotationX = -(20f + ((spannedIndex - index ) * 30f))
+    val rotationX = -25f * (spannedIndex - index)
+    val rotationXY =  (spannedIndex - index)
     return rotationX
 }
 
 private fun calculateItemAlpha(listState: LazyListState, itemCount: Int, index: Int): Float {
 
-    val firstVisibleItem = listState.layoutInfo.visibleItemsInfo.firstOrNull()?.index ?: 0
+    val firstVisibleItem = listState.firstVisibleItemIndex
     val spannedIndex =
         firstVisibleItem - 1 + (listState.layoutInfo.visibleItemsInfo.size / 2).toInt()
 
+    Log.d("spanned","$spannedIndex")
 
-    val rotationX = 1f - (0.1f + 0.2f * (spannedIndex - index).absoluteValue)
+    val rotationX = 1f - (0.3f * (spannedIndex - index).absoluteValue)
     return rotationX
 }
