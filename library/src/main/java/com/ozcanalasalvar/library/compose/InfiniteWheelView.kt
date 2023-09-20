455a4b8c011a5c717a8f519acaca427b8e4b11df
ozcanalasalvar <ozcanalasalvarr@gmail.com>
2023-09-20 12:20:00 +0300

    wheel view ui

--- library/src/main/java/com/ozcanalasalvar/library/compose/InfiniteWheelView.kt
+++ library/src/main/java/com/ozcanalasalvar/library/compose/InfiniteWheelView.kt
@@ -2,6 +2,7 @@ package com.ozcanalasalvar.library.compose
 
 import android.util.Log
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
@@ -15,9 +16,12 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.DpSize
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import kotlinx.coroutines.launch
 import kotlin.math.absoluteValue
 
@@ -25,14 +29,15 @@ import kotlin.math.absoluteValue
 @Composable
 fun InfiniteWheelView(
     modifier: Modifier = Modifier,
-    size: DpSize = DpSize(200.dp, 200.dp),
+    size: DpSize = DpSize(256.dp, 200.dp),
     startIndex: Int = 3,
     itemCount: Int = 28,//TODO will be array of element,
-    rowOffsetCount: Int = 2,
+    rowOffsetCount: Int = 3,
 ) {
 
-    val lazyListState = rememberLazyListState(startIndex + (itemCount * 10))
+    val lazyListState = rememberLazyListState(startIndex + (itemCount * 1000))
     val isScrollInProgress = lazyListState.isScrollInProgress
+    val spannedIndex = remember { derivedStateOf { lazyListState.firstVisibleItemIndex } }
 
     val rowCount = ((rowOffsetCount * 2) + 1)
 
@@ -63,24 +68,28 @@ fun InfiniteWheelView(
                 .height(size.height)
                 .width(size.width),
             state = lazyListState,
-            // contentPadding = PaddingValues(vertical = size.height / 3)
+//             contentPadding = PaddingValues(vertical = size.height / 3)
         ) {
 
 
             items(Int.MAX_VALUE) {
 
-                val rotateDegree = calculateItemRotation(lazyListState, itemCount = itemCount, it)
+                val rotateDegree = calculateItemRotation(spannedIndex.value + rowOffsetCount, it)
                 Box(
                     modifier = Modifier
                         .height(size.height / rowCount)
-                        .width(size.width),
-//                        .graphicsLayer {
-//                            this.rotationX = calculateItemRotation(lazyListState, itemCount, it)
-//                        },
+                        .width(size.width)
+                        .graphicsLayer {
+                            this.rotationX = rotateDegree
+                        }
+                        .alpha(calculateItemAlpha(spannedIndex.value + rowOffsetCount, it)),
                     contentAlignment = Alignment.Center,
                 ) {
 
-                    Text(text = (it % itemCount + 1).toString())
+                    Text(
+                        text = if ((it % itemCount + 1) <10) " ${(it % itemCount + 1)}" else (it % itemCount + 1).toString(),
+//                        fontSize = (18 - (spannedIndex.value + rowOffsetCount - it).absoluteValue).sp,
+                    )
                     //Text(text = (it).toString())
                 }
 
@@ -98,35 +107,39 @@ private fun calculateSnappedItemIndex(listState: LazyListState, height: Dp): Int
     var index = currentItem?.index ?: 0
 
     if (currentItem?.offset != 0) {
-        if (currentItem != null && currentItem.offset <= -height.value * 4 / 10) {
+        if (currentItem != null && currentItem.offset <= -height.value * 3 / 10) {
             index++
         }
     }
+    Log.d("spanned", "$index")
     return index
 }
 
-private fun calculateItemRotation(listState: LazyListState, itemCount: Int, index: Int): Float {
+@Composable
+private fun calculateItemRotation(spannedIndex: Int, index: Int): Float {
 
-    val firstVisibleItem = listState.firstVisibleItemIndex
-    val spannedIndex =
-        firstVisibleItem - 1 + (listState.layoutInfo.visibleItemsInfo.size / 2).toInt()
-   // Log.d("spanned","$spannedIndex")
-    Log.d("spanned","${(spannedIndex - index )}")
+    /*  val layoutInfo = remember { derivedStateOf { listState.layoutInfo } }.value
+      val firstVisibleItem = remember { derivedStateOf { listState.firstVisibleItemIndex } }.value
+
+      val spannedIndex =
+          layoutInfo.visibleItemsInfo.firstOrNull()?.index?:0 + 2
+     // Log.d("spanned","$spannedIndex")
+     // Log.d("spanned","${(spannedIndex - index )}")*/
 
 //    val rotationX = -(20f + ((spannedIndex - index ) * 30f))
-    val rotationX = -25f * (spannedIndex - index)
-    val rotationXY =  (spannedIndex - index)
+    val rotationX = -15f * (spannedIndex - index)
+    val rotationXY = (spannedIndex - index)
     return rotationX
 }
 
-private fun calculateItemAlpha(listState: LazyListState, itemCount: Int, index: Int): Float {
+@Composable
+private fun calculateItemAlpha(spannedIndex: Int, index: Int): Float {
+    /* val layoutInfo = remember { derivedStateOf { listState.layoutInfo } }.value
+     val firstVisibleItem = remember { derivedStateOf { listState.firstVisibleItemIndex } }.value
+     val spannedIndex =
+         layoutInfo.visibleItemsInfo.firstOrNull()?.index?:0 + 2*/
 
-    val firstVisibleItem = listState.firstVisibleItemIndex
-    val spannedIndex =
-        firstVisibleItem - 1 + (listState.layoutInfo.visibleItemsInfo.size / 2).toInt()
-
-    Log.d("spanned","$spannedIndex")
 
     val rotationX = 1f - (0.3f * (spannedIndex - index).absoluteValue)
-    return rotationX
+    return rotationX.absoluteValue
 }
--- sample/src/main/java/com/ozcanalasalvar/sample/ComposeActivity.kt
+++ sample/src/main/java/com/ozcanalasalvar/sample/ComposeActivity.kt
@@ -3,12 +3,22 @@ package com.ozcanalasalvar.sample
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Divider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.ozcanalasalvar.library.compose.InfiniteWheelView
 import com.ozcanalasalvar.library.compose.WheelView
 import com.ozcanalasalvar.sample.ui.theme.DatePickerTheme
@@ -19,17 +29,22 @@ class ComposeActivity : ComponentActivity() {
         setContent {
             DatePickerTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(
+                Box(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                    contentAlignment = Alignment.Center
                 ) {
+
+                    Box(modifier = Modifier.border( width = 2.dp,
+                        color = Color.Red,).height(30.dp).fillMaxWidth())
+
                     InfiniteWheelView(modifier = Modifier)
+
+
                 }
             }
         }
     }
 }
-
 
 
 @Preview
