035703ec98b6398acea7f11e1180676652ae3469
ozcanalasalvar <ozcanalasalvarr@gmail.com>
2023-09-21 14:40:07 +0300

    ComposeDate picker

---
+++ library/src/main/java/com/ozcanalasalvar/library/compose/DatePicker.kt
@@ -0,0 +1,156 @@
+package com.ozcanalasalvar.library.compose
+
+import android.util.Log
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.IntrinsicSize
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.DpSize
+import androidx.compose.ui.unit.dp
+
+
+@Composable
+fun DatePicker() {
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(IntrinsicSize.Max)
+            .background(Color.White),
+        contentAlignment = Alignment.Center
+    ) {
+
+        var spannedIndexOfDay by remember {
+            mutableStateOf(3)
+        }
+
+        val months = listOf<String>(
+            "January",
+            "February",
+            "March",
+            "April",
+            "May",
+            "June",
+            "July",
+            "August",
+            "September",
+            "October",
+            "November",
+            "December"
+        )
+
+        val days = mutableListOf<String>().apply {
+            for (index in 1..31) {
+                add(index.toString())
+            }
+        }
+
+        val years = mutableListOf<String>().apply {
+            for (index in 1..50) {
+                add((1995 + index).toString())
+            }
+        }
+
+
+        Row(modifier = Modifier.fillMaxSize()) {
+
+
+            InfiniteWheelView(
+                modifier = Modifier,
+                size = DpSize(150.dp, 225.dp),
+                startIndex = spannedIndexOfDay,
+                itemCount = days.size,
+                rowOffset = 4,
+                onFocusItem = {
+                    Log.d("SpannedIndex", "$it")
+                }, content = {
+                    Text(
+                        text = days[it],
+                        textAlign = TextAlign.End,
+                        modifier = Modifier.width(100.dp)
+                    )
+                })
+
+            InfiniteWheelView(
+                modifier = Modifier,
+                size = DpSize(150.dp, 225.dp),
+                startIndex = 0,
+                itemCount = months.size,
+                rowOffset = 4,
+                onFocusItem = {
+
+                }, content = {
+                    Text(
+                        text = months[it],
+                        textAlign = TextAlign.Start,
+                        modifier = Modifier.width(100.dp)
+                    )
+                })
+
+            InfiniteWheelView(
+                modifier = Modifier,
+                size = DpSize(150.dp, 225.dp),
+                startIndex = 0,
+                itemCount = years.size,
+                rowOffset = 4,
+                onFocusItem = {
+
+                }, content = {
+                    Text(
+                        text = years[it],
+                        textAlign = TextAlign.Start,
+                        modifier = Modifier.width(100.dp)
+                    )
+                })
+        }
+
+
+        Column(
+            Modifier
+                .fillMaxSize()
+        ) {
+
+            Box(
+                modifier = Modifier
+                    .height(97.dp)
+                    .fillMaxWidth()
+                    .background(Color(0x80FFFFFF)),
+            )
+
+            Box(
+                modifier = Modifier
+                    .border(
+                        width = 0.5.dp,
+                        color = Color(0x80000000),
+                    )
+                    .height(31.dp)
+                    .fillMaxWidth()
+            )
+
+
+            Box(
+                modifier = Modifier
+                    .height(97.dp)
+                    .fillMaxWidth()
+                    .background(Color(0x80FFFFFF)),
+            )
+        }
+
+
+    }
+}
--- library/src/main/java/com/ozcanalasalvar/library/compose/InfiniteWheelView.kt
+++ library/src/main/java/com/ozcanalasalvar/library/compose/InfiniteWheelView.kt
@@ -1,29 +1,27 @@
 package com.ozcanalasalvar.library.compose
 
 import android.util.Log
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyItemScope
 import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.foundation.lazy.rememberLazyListState
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.graphicsLayer
-import androidx.compose.ui.text.font.FontStyle
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.DpSize
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import kotlinx.coroutines.launch
-import kotlin.math.absoluteValue
 
 
 @Composable
@@ -32,27 +30,32 @@ fun InfiniteWheelView(
     size: DpSize = DpSize(256.dp, 200.dp),
     startIndex: Int = 3,
     itemCount: Int = 28,//TODO will be array of element,
-    rowOffsetCount: Int = 3,
+    rowOffset: Int = 4,
+    content: @Composable LazyItemScope.(index: Int) -> Unit,
+    onFocusItem: (Int) -> Unit,
 ) {
 
     val lazyListState = rememberLazyListState(startIndex + (itemCount * 1000))
     val isScrollInProgress = lazyListState.isScrollInProgress
-    val spannedIndex = remember { derivedStateOf { lazyListState.firstVisibleItemIndex } }
-
+    val rowOffsetCount = maxOf(1, minOf(rowOffset, 4))
     val rowCount = ((rowOffsetCount * 2) + 1)
+
+    val spannedIndex =
+        remember { derivedStateOf { lazyListState.firstVisibleItemIndex + rowOffsetCount } }
 
     val coroutineScope = rememberCoroutineScope()
 
     LaunchedEffect(key1 = isScrollInProgress) {
-        coroutineScope.launch {
-            if (!isScrollInProgress) {
-                calculateSnappedItemIndex(lazyListState, size.height).let {
-                    if (lazyListState.firstVisibleItemScrollOffset != 0) lazyListState.animateScrollToItem(
-                        it, 0
-                    )
+        if (!isScrollInProgress) {
+            calculateSnappedItemIndex(lazyListState, size.height).let {
+                val focusedIndex = (it + rowOffsetCount) % itemCount
+                onFocusItem(focusedIndex)
+                if (lazyListState.firstVisibleItemScrollOffset != 0) {
+                    coroutineScope.launch {
+                        lazyListState.animateScrollToItem(it, 0)
+                    }
                 }
             }
-
         }
     }
 
@@ -68,33 +71,55 @@ fun InfiniteWheelView(
                 .height(size.height)
                 .width(size.width),
             state = lazyListState,
-//             contentPadding = PaddingValues(vertical = size.height / 3)
+            // contentPadding = PaddingValues(vertical = size.height / rowCount*rowOffsetCount)
         ) {
 
 
             items(Int.MAX_VALUE) {
 
-                val rotateDegree = calculateItemRotation(spannedIndex.value + rowOffsetCount, it)
+                val rotateDegree = calculateItemRotation(spannedIndex.value, it)
                 Box(
                     modifier = Modifier
                         .height(size.height / rowCount)
                         .width(size.width)
                         .graphicsLayer {
                             this.rotationX = rotateDegree
-                        }
-                        .alpha(calculateItemAlpha(spannedIndex.value + rowOffsetCount, it)),
+                        },
                     contentAlignment = Alignment.Center,
                 ) {
-
-                    Text(
-                        text = if ((it % itemCount + 1) <10) " ${(it % itemCount + 1)}" else (it % itemCount + 1).toString(),
-//                        fontSize = (18 - (spannedIndex.value + rowOffsetCount - it).absoluteValue).sp,
-                    )
-                    //Text(text = (it).toString())
+                    content(it % itemCount)
                 }
 
             }
         }
+
+
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(
+                    brush = Brush.verticalGradient(
+                        colors = listOf(
+                            Color(0xE6FFFFFF),
+                            Color(0xE6FFFFFF),
+                            Color(0xB3FFFFFF),
+                            Color(0xB3FFFFFF),
+                            Color(0x80FFFFFF),
+                            Color(0x80FFFFFF),
+                            Color.Transparent,
+                            Color.Transparent,
+                            Color.Transparent,
+                            Color.Transparent,
+                            Color(0x80FFFFFF),
+                            Color(0x80FFFFFF),
+                            Color(0xB3FFFFFF),
+                            Color(0xB3FFFFFF),
+                            Color(0xE6FFFFFF),
+                            Color(0xE6FFFFFF),
+                        )
+                    )
+                ),
+        ) {}
 
     }
 
@@ -117,29 +142,5 @@ private fun calculateSnappedItemIndex(listState: LazyListState, height: Dp): Int
 
 @Composable
 private fun calculateItemRotation(spannedIndex: Int, index: Int): Float {
-
-    /*  val layoutInfo = remember { derivedStateOf { listState.layoutInfo } }.value
-      val firstVisibleItem = remember { derivedStateOf { listState.firstVisibleItemIndex } }.value
-
-      val spannedIndex =
-          layoutInfo.visibleItemsInfo.firstOrNull()?.index?:0 + 2
-     // Log.d("spanned","$spannedIndex")
-     // Log.d("spanned","${(spannedIndex - index )}")*/
-
-//    val rotationX = -(20f + ((spannedIndex - index ) * 30f))
-    val rotationX = -15f * (spannedIndex - index)
-    val rotationXY = (spannedIndex - index)
-    return rotationX
-}
-
-@Composable
-private fun calculateItemAlpha(spannedIndex: Int, index: Int): Float {
-    /* val layoutInfo = remember { derivedStateOf { listState.layoutInfo } }.value
-     val firstVisibleItem = remember { derivedStateOf { listState.firstVisibleItemIndex } }.value
-     val spannedIndex =
-         layoutInfo.visibleItemsInfo.firstOrNull()?.index?:0 + 2*/
-
-
-    val rotationX = 1f - (0.3f * (spannedIndex - index).absoluteValue)
-    return rotationX.absoluteValue
+    return -15f * (spannedIndex - index)
 }
